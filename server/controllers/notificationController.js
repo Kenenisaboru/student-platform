2eb9ecc35828bca9f6fedda566b1fd0faba64489
kenenisa <kenenisaboru998@gmail.com>
2026-03-12 23:00:04 +0000

    Add server/controllers/notificationController.js

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,25 @@
+const Notification = require('../models/Notification');
+
+exports.getNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({ recipient: req.user._id })
+      .populate('sender', 'name profilePicture')
+      .populate('post', 'title')
+      .sort({ createdAt: -1 });
+    res.json(notifications);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.markAsRead = async (req, res) => {
+  try {
+    await Notification.updateMany(
+      { recipient: req.user._id, read: false },
+      { $set: { read: true } }
+    );
+    res.json({ message: 'Notifications marked as read' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
