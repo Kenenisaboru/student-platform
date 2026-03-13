ac806e44c61daae8c8f74aed21606587370806ec
kenenisa <kenenisaboru998@gmail.com>
2026-03-13 00:18:55 +0000

    Update .\server\controllers\userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,3 +69,12 @@ exports.getAllUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
