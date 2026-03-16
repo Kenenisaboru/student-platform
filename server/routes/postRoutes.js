828ae681774671227614bd893c89786d0ab80bb0
kenenisa <kenenisaboru998@gmail.com>
2026-03-16 23:53:19 +0000

    feat(routes): Protect image upload routing strings with Cloudinary middleware

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -6,11 +6,19 @@ const {
   getPostById, 
   updatePost, 
   deletePost, 
-  likePost 
+  likePost,
+  getTrendingTags,
+  getCommunityStats
 } = require('../controllers/postController');
 const { protect } = require('../middleware/authMiddleware');
+const { upload } = require('../config/cloudinary');
 
-router.post('/', protect, createPost);
+// Stats routes (before /:id to avoid conflicts)
+router.get('/trending-tags', protect, getTrendingTags);
+router.get('/community-stats', protect, getCommunityStats);
+
+// Post CRUD with image upload support
+router.post('/', protect, upload.single('image'), createPost);
 router.get('/', getPosts);
 router.get('/:id', getPostById);
 router.put('/:id', protect, updatePost);
