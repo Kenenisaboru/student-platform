fe0f8ecf715401ddf432bcbde573ff836431d3e2
kenenisa <kenenisaboru998@gmail.com>
2026-03-16 01:23:02 +0000

    Update user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,11 +8,18 @@ const {
   deleteUser
 } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
+const { upload } = require('../config/cloudinary');
 
 router.get('/search', protect, searchUsers);
 router.get('/all', protect, admin, getAllUsers);
 router.get('/:id', protect, getProfile);
 router.put('/profile', protect, updateProfile);
+router.post('/upload-profile', protect, upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+  res.json({ url: req.file.path });
+});
 router.delete('/:id', protect, admin, deleteUser);
 
 module.exports = router;
