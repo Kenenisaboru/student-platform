21d8e2d130df1672a87ff2e9e01a31cb368040ee
kenenisa <kenenisaboru998@gmail.com>
2026-03-19 01:47:53 +0000

    feat(routes): map comment interaction API endpoints

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const { 
   createComment, 
   getCommentsByPost, 
-  deleteComment 
+  deleteComment,
+  likeComment
 } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware');
+const { validate, createCommentRules, paginationRules } = require('../middleware/validation');
 
-router.post('/:postId', protect, createComment);
-router.get('/:postId', getCommentsByPost);
+router.post('/:postId', protect, createCommentRules, validate, createComment);
+router.get('/:postId', paginationRules, validate, getCommentsByPost);
 router.delete('/:id', protect, deleteComment);
+router.post('/:id/like', protect, likeComment);
 
 module.exports = router;
