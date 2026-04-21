ae91f1e456488e96e7f94ba833a2c10f7b7f878c
kenenisa <kenenisaboru998@gmail.com>
2026-04-21 17:47:41 +0100

    chore: update cloudinary storage config

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'student-platform/profiles',
+    folder: 'communication_platform/profiles',
     allowed_formats: ['jpg', 'png', 'jpeg'],
     transformation: [{ width: 500, height: 500, crop: 'limit' }],
   },
