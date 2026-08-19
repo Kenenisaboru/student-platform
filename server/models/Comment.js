6a558e677b785cd004441e5e118fef2c7b1ad9b8
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-19 07:03:59 -0700

    Update server/models/Comment.js

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -27,4 +27,7 @@ const commentSchema = new mongoose.Schema({
   }]
 }, { timestamps: true });
 
+commentSchema.index({ post: 1, parentComment: 1 });
+commentSchema.index({ post: 1, createdAt: -1 });
+
 module.exports = mongoose.model('Comment', commentSchema);
