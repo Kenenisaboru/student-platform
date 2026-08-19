4a64d4b25f1b5a7c96f578e5df366e92903e4294
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-19 07:04:02 -0700

    Update server/routes/postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -9,15 +9,20 @@ const {
   likePost,
   votePoll,
   getTrendingTags,
-  getCommunityStats
+  getCommunityStats,
+  repostPost,
+  getRepostedPosts
 } = require('../controllers/postController');
 const { secure } = require('../middleware/authMiddleware');
 const { upload } = require('../config/cloudinary');
-const { validate, createPostRules, updatePostRules, paginationRules } = require('../middleware/validation');
+const { validate, createPostRules, updatePostRules, paginationRules, repostRules } = require('../middleware/validation');
 
 // Stats routes (before /:id to avoid conflicts)
 router.get('/trending-tags', secure, getTrendingTags);
 router.get('/community-stats', secure, getCommunityStats);
+
+// User reposts (before /:id to avoid conflicts)
+router.get('/user/:userId/reposts', getRepostedPosts);
 
 // Post CRUD with image upload support & validation
 router.post('/', secure, upload.single('image'), createPostRules, validate, createPost);
@@ -26,6 +31,7 @@ router.get('/:id', getPostById);
 router.put('/:id', secure, updatePostRules, validate, updatePost);
 router.delete('/:id', secure, deletePost);
 router.post('/:id/like', secure, likePost);
+router.post('/:id/repost', secure, repostRules, validate, repostPost);
 router.post('/:id/vote', secure, votePoll);
 
 module.exports = router;
