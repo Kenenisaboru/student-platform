34d26c52e0e71e4701a0ba811e20233b586e05cf
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-19 07:04:01 -0700

    Update server/routes/messageRoutes.js

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -5,13 +5,21 @@ const {
   getConversations,
   getMessages,
   sendMessage,
-  getUnreadCount
+  getUnreadCount,
+  createGroupConversation,
+  addParticipants,
+  removeParticipant,
+  updateConversation
 } = require('../controllers/messageController');
 const { secure } = require('../middleware/authMiddleware');
-const { validate, sendMessageRules, paginationRules } = require('../middleware/validation');
+const { validate, sendMessageRules, paginationRules, createGroupRules, updateConversationRules } = require('../middleware/validation');
 
 router.get('/conversations', secure, getConversations);
 router.get('/unread-count', secure, getUnreadCount);
+router.post('/conversations/group', secure, createGroupRules, validate, createGroupConversation);
+router.put('/conversations/:id/participants', secure, addParticipants);
+router.delete('/conversations/:id/participants', secure, removeParticipant);
+router.put('/conversations/:id', secure, updateConversationRules, validate, updateConversation);
 router.post('/conversation/:userId', secure, getOrCreateConversation);
 router.get('/:conversationId', secure, paginationRules, validate, getMessages);
 router.post('/:conversationId', secure, sendMessageRules, validate, sendMessage);
