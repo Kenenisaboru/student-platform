237056e27fded86e0abb83017997b7cfdea826c2
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-19 07:03:59 -0700

    Update server/models/Conversation.js

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['direct', 'group', 'broadcast'],
+    default: 'direct'
+  },
+  name: {
+    type: String,
+    trim: true
+  },
+  avatar: {
+    type: String
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
   participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  pinnedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  mutedBy: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }],
